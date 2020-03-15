bc22ff1feec7d70a5b956de8dd6a3c2ed51def55
martin0726 <60959558+martin0726@users.noreply.github.com>
2020-03-15 19:47:14 +0800

    cleaned urls.py at froyo

--- froyo/urls.py
+++ froyo/urls.py
@@ -4,17 +4,17 @@ from .views import IngredientsListView, IngredientsDetailView, IngredientsUpdate
  
 urlpatterns = [
 		url(r'^ingredients_list$', IngredientsListView.as_view(), name='Ingredients_List_show'),
-		url(r'^ingredients_detail/$', IngredientsDetailView.as_view(), name='Ingredients_Detail_show'),
-		url(r'^ingredients_update/$', IngredientsUpdateView.as_view(), name='Ingredients_Update_show'),
-		url(r'^ingredients_create/$', IngredientsCreateView.as_view(), name='Ingredients_Create_show'),
+		url(r'^ingredients_detail$', IngredientsDetailView.as_view(), name='Ingredients_Detail_show'),
+		url(r'^ingredients_update$', IngredientsUpdateView.as_view(), name='Ingredients_Update_show'),
+		url(r'^ingredients_create$', IngredientsCreateView.as_view(), name='Ingredients_Create_show'),
 
-		url(r'^recipes_list/$', RecipesListView.as_view(), name='Recipes_List_show'),
-		url(r'^recipes_detail/$', RecipesDetailView.as_view(), name='Recipes_Detail_show'),
-		url(r'^recipes_update/$', RecipesUpdateView.as_view(), name='Recipes_Update_show'),
-		url(r'^recipes_create/$', RecipesCreateView.as_view(), name='Recipes_Create_show'),
+		url(r'^recipes_list$', RecipesListView.as_view(), name='Recipes_List_show'),
+		url(r'^recipes_detail$', RecipesDetailView.as_view(), name='Recipes_Detail_show'),
+		url(r'^recipes_update$', RecipesUpdateView.as_view(), name='Recipes_Update_show'),
+		url(r'^recipes_create$', RecipesCreateView.as_view(), name='Recipes_Create_show'),
 
-		url(r'^orders_list/$', OrdersListView.as_view(), name='Orders_List_show'),
-		url(r'^orders_detail/$', OrdersDetailView.as_view(), name='Orders_Detail_show'),
-		url(r'^orders_update/$', OrdersUpdateView.as_view(), name='Orders_Update_show'),
-		url(r'^orders_create/$', OrdersCreateView.as_view(), name='Orders_Create_show'),
+		url(r'^orders_list$', OrdersListView.as_view(), name='Orders_List_show'),
+		url(r'^orders_detail$', OrdersDetailView.as_view(), name='Orders_Detail_show'),
+		url(r'^orders_update$', OrdersUpdateView.as_view(), name='Orders_Update_show'),
+		url(r'^orders_create$', OrdersCreateView.as_view(), name='Orders_Create_show'),
 ]
