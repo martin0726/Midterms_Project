9c086af5dbd836c6555226fa976e7261872223c2
martin0726 <60959558+martin0726@users.noreply.github.com>
2020-03-15 16:45:40 +0800

    Cleand up views.py

--- froyo/views.py
+++ froyo/views.py
@@ -6,49 +6,49 @@ from.models import *
 
 
 class IngredientsListView(generic.ListView):
-	template_name = 'ingredients/ingredients_list.html'
+	template_name = 'ingredients_list.html'
 
 
 class IngredientsDetailView(generic.DetailView):
-	template_name = 'ingredients/ingredients_detail.html'
+	template_name = 'ingredients_detail.html'
 
 
 class IngredientsUpdateView(generic.UpdateView):
-	template_name = 'ingredients/ingredients_update_form.html'
+	template_name = 'ingredients_update_form.html'
 
 
 class IngredientsCreateView(generic.CreateView):
-	template_name = 'ingredients/ingredients_create_form.html'
+	template_name = 'ingredients_create_form.html'
 
 
 class RecipesListView(generic.ListView):
-	template_name = 'recipes/recipes_list.html'
+	template_name = 'recipes_list.html'
 
 
 class RecipesDetailView(generic.DetailView):
-	template_name = 'recipes/recipes_detail.html'
+	template_name = 'recipes_detail.html'
 
 
 class RecipesUpdateView(generic.UpdateView):
-	template_name = 'recipes/recipes_update_form.html'
+	template_name = 'recipes_update_form.html'
 
 
 class RecipesCreateView(generic.CreateView):
-	template_name = 'recipes/recipes_create_form.html'
+	template_name = 'recipes_create_form.html'
 
 
 class OrdersListView(generic.ListView):
-	template_name = 'orders/orders_list.html'
+	template_name = 'orders_list.html'
 
 
 class OrdersDetailView(generic.DetailView):
-	template_name = 'orders/orders_detail.html'
+	template_name = 'orders_detail.html'
 
 
 class OrdersUpdateView(generic.UpdateView):
-	template_name = 'orders/orders_update_form.html'
+	template_name = 'orders_update_form.html'
 
 
 class OrdersCreateView(generic.CreateView):
-	template_name = 'orders/orders_create_form.html'
+	template_name = 'orders_create_form.html'
 # Create your views here.
